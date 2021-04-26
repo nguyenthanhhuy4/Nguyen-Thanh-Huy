f57cf6f2c43bff647fe3b3cebe8ff4c966f80c4c
NguyenThanhHuy <huycoc4699@gmail.com>
2021-04-26 18:07:27 +0700

    hw14

---
+++ hw/msita.js
@@ -0,0 +1,35 @@
+function showdesign() {
+	if(document.getElementById('DESIGN').style.display == ('none')){
+		document.getElementById('DESIGN').style.display = ('block')
+}else
+	{ document.getElementById('DESIGN').style.display = ('none')
+}
+}
+function showmarketing() {
+	if(document.getElementById('MARKETING').style.display == ('none')){
+		document.getElementById('MARKETING').style.display = ('block')
+}else
+	{ document.getElementById('MARKETING').style.display = ('none')
+}
+}
+function showmos() {
+	if(document.getElementById('MOS').style.display == ('none')){
+		document.getElementById('MOS').style.display = ('block')
+}else
+	{ document.getElementById('MOS').style.display = ('none')
+}
+}
+function shownormal() {
+	if(document.getElementById('normal').style.display == ('none')){
+		document.getElementById('normal').style.display = ('block')
+}else
+	{ document.getElementById('normal').style.display = ('none')
+}
+}
+function showadvanced() {
+	if(document.getElementById('advanced').style.display == ('none')){
+		document.getElementById('advanced').style.display = ('block')
+}else
+	{ document.getElementById('advanced').style.display = ('none')
+}
+}
